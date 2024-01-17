cedfa3c4efe45f4122222c5ac339eaa6f2b39b95
Zhdan <ivan.fran.0803.zz@gmail.com>
2024-01-17 01:14:19 +0100

    Removed over 100mb files

--- main.tf
+++ main.tf
@@ -57,16 +57,60 @@ resource "aws_security_group" "sg_ec2" {
 
 resource "aws_instance" "borse_instance" {
   ami                    = "ami-0faab6bdbac9486fb"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.small" #t2.micro
   key_name               = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
 
   tags = {
     Name = "borse_instance"
   }
-
+  
+  timeouts {
+    create = "7m"
+  }
+ 
   root_block_device {
-    volume_size = 30
+    volume_size = 10
     volume_type = "gp2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y docker.io",
+      "sudo usermod -aG docker $USER",
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.key_name)
+      host        = aws_instance.borse_instance.public_ip
+      timeout = "2m"
+    }
+  }
+  #seconda connection per evitare i problemi dovuti all'utenza
+  provisioner "remote-exec" {
+    inline = [
+      "sudo service docker restart",
+      "sudo systemctl daemon-reload",
+      "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo systemctl daemon-reload",
+      "git clone https://github.com/saleor/saleor-platform.git",
+      "cd saleor-platform/",
+      "docker-compose build",
+      "docker-compose run --rm api python3 manage.py migrate",
+      "docker-compose run --rm api python3 manage.py populatedb --createsuperuser",
+      "docker-compose up -d"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.key_name)
+      host        = aws_instance.borse_instance.public_ip
+      timeout = "3m"
+    }
   }
 }
